9609417304a94907e85e565e40b7a1af54eb0c33
james hardy <james@ipebble.co.uk>
2019-10-14 16:37:35 +0100

    added project

---
+++ GBG/scripts/scripts.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#0F2980', '#ffd200', '#c1c1c1'],
+        anchors: ['firstSection', 'secondSection', 'thirdSection'],
+        menu: '.main-nav ul',
+        licenseKey: '6A53BCF6-46E5458A-A28A48F7-E821B8CA'
+    });
+});
